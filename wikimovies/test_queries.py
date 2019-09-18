b5bfdc72cd92eb4d173ba4c36f0d24969344bc22
Diego Amicabile <diego.amicabile@gmail.com>
2019-09-18 08:43:33 +0200

    feat: add tests for verifying most versatile and prolific people

--- wikimovies/test_queries.py
+++ wikimovies/test_queries.py
@@ -1,5 +1,5 @@
 
-all_works_from_someone = """
+ALL_WORKS_FROM_SOMEONE_QUERY = """
 SELECT H.human_name,C.creative_work_name, C.creative_work_type, P.role_name 
 FROM creative_works C, humans H, participations P
 WHERE P.human_id = H.human_id
@@ -7,62 +7,57 @@ AND C.creative_work_id = P.creative_work_id
 AND H.human_name like '%{}%'
 """
 
-
-
-most_creative_works = """
-SELECT H.human_id, H.human_name, C.creative_work_type, COUNT(*) AS CNT FROM creative_works C, humans H, participations P where
-P.human_id = H.human_id AND C.creative_work_id = P.creative_work_id
+MOST_CREATIVE_WORKS_QUERY = """
+SELECT H.human_id, H.human_name, C.creative_work_type, COUNT(*) AS CNT 
+FROM creative_works C, humans H, participations P 
+WHERE P.human_id = H.human_id 
+AND C.creative_work_id = P.creative_work_id
 GROUP BY (H.human_name, H.human_id, C.creative_work_type)
 ORDER BY CNT DESC
 """
 
-partition_creative_works = """
-SELECT human_id, human_name, creative_work_type, cnt, sum(cnt) over (PARTITION BY human_id) as snct FROM (
-                                                    SELECT H.human_id,
-                                                           H.human_name,
-                                                           C.creative_work_type,
-                                                           COUNT(*) AS CNT
-                                                    FROM creative_works C,
-                                                         humans H,
-                                                         participations P
-                                                    where P.human_id = H.human_id
-                                                      AND C.creative_work_id = P.creative_work_id
-                                                    GROUP BY (H.human_name, H.human_id, C.creative_work_type)
-                                                    ORDER BY CNT DESC
-                                                ) T
-ORDER BY snct desc
+MOST_CREATIVE_HUMAN = "Ilaiyaraaja"
+
+MOST_VERSATILE_WORKS_QUERY = """
+SELECT human_id, human_name, creative_work_type, cnt, 
+SUM(cnt) OVER (PARTITION BY human_id) as snct, 
+COUNT(cnt) OVER (PARTITION BY human_id) ccnt FROM (
+    SELECT H.human_id,
+           H.human_name,
+           C.creative_work_type,
+           COUNT(*) AS CNT
+    FROM creative_works C,
+         humans H,
+         participations P
+    WHERE P.human_id = H.human_id
+      AND C.creative_work_id = P.creative_work_id
+    GROUP BY (H.human_name, H.human_id, C.creative_work_type)
+    ORDER BY CNT DESC
+) CREATIVE_WORKS
+ORDER BY ccnt DESC, snct DESC
 """
 
+MOST_VERSATILE_HUMAN_WORKS = "Leonard Nimoy"
 
-all_works_from_ogg = """
-SELECT H.human_name,C.creative_work_name, C.creative_work_type, P.role_name FROM creative_works C,
-                                                         humans H,
-                                                         participations P
-                                                    where P.human_id = H.human_id
-                                                      AND C.creative_work_id = P.creative_work_id
+MOST_VERSATILE_ROLES_QUERY = """
+SELECT human_id, human_name, role_name, cnt, 
+SUM(cnt) over (PARTITION BY human_id) as snct, 
+COUNT(cnt) over (PARTITION BY human_id) ccnt FROM (
+    SELECT H.human_id,
+           H.human_name,
+           P.role_name,
+           COUNT(*) AS CNT
+    FROM humans H,
+         participations P
+    where P.human_id = H.human_id
 
-                                                      AND H.human_name like  '%Steven Ogg%'
+    GROUP BY (H.human_name, H.human_id, P.role_name)
+    ORDER BY CNT DESC
+) CREATIVE_ROLES
+ORDER BY ccnt desc, snct desc
 """
 
-all_works_from_villaggio = """
-SELECT H.human_name,C.creative_work_name, C.creative_work_type, P.role_name FROM creative_works C,
-                                                         humans H,
-                                                         participations P
-                                                    where P.human_id = H.human_id
-                                                      AND C.creative_work_id = P.creative_work_id
-
-                                                      AND H.human_name like  '%Paolo Villaggio%'
-"""
-
-all_works_from_eddie_murphy = """
-SELECT H.human_name,C.creative_work_name, C.creative_work_type, P.role_name FROM creative_works C,
-                                                         humans H,
-                                                         participations P
-                                                    where P.human_id = H.human_id
-                                                      AND C.creative_work_id = P.creative_work_id
-
-                                                      AND H.human_name like  '%Eddie Murphy%'
-"""
+MOST_VERSATILE_HUMAN_ROLES = "Robert Rodriguez"
 
 
 
@@ -85,12 +80,26 @@ all_test_count_tables = [("humans", 2000000),
 
 
 all_verify_works_from_someone = \
-    [("Adriano Celentano", 50, {"MOVIE", "SONG", "TVSHOW"}),
-    ("Steven Ogg", 3, {"MOVIE", "VIDEOGAME", "TVSHOW"}),
-    ("Eddie Murphy", 40, {"ANIMATEDMOVIE", "MOVIE", "SONG"}),
-    ("Bryan Cranston", 40, {"ANIMATEDMOVIE", "MOVIE", "TVSHOW"}),
-    ("Ashley Johnson", 30, {"ANIMATEDMOVIE", "MOVIE", "TVSHOW", "VIDEOGAME"})]
+    [("Adriano Celentano", 50, {"MOVIE", "SONG", "TVSHOW"}, {"cast member", "performer"}),
+    ("Steven Ogg", 3, {"MOVIE", "VIDEOGAME", "TVSHOW"}, {"cast member", "voice actor"}),
+    ("Paolo Villaggio", 40, {"MOVIE", "BOOK"}, {"cast member", "screenwriter", "author"}),
+    ("Eddie Murphy", 40, {"ANIMATEDMOVIE", "MOVIE", "SONG"}, {"cast member", "performer", "voice actor"}),
+    ("Bryan Cranston", 40, {"ANIMATEDMOVIE", "MOVIE", "TVSHOW"}, {"cast member", "voice actor", "director"}),
+    ("Ashley Johnson", 30, {"ANIMATEDMOVIE", "MOVIE", "TVSHOW", "VIDEOGAME"}, {"cast member", "voice actor"})]
 
+
+def execute_most_creative(cur):
+
+    def compare_human_with_queryresult(cur, query, target_human, quality):
+        cur.execute(query)
+        result = cur.fetchall()
+        human_creative = result[0][1]
+        print("Asserting most {} human {} == {}".format(quality, human_creative, target_human))
+        assert (target_human == human_creative)
+
+    compare_human_with_queryresult(cur, MOST_CREATIVE_WORKS_QUERY, MOST_CREATIVE_HUMAN, "creative")
+    compare_human_with_queryresult(cur, MOST_VERSATILE_WORKS_QUERY, MOST_VERSATILE_HUMAN_WORKS, "versatile in works")
+    compare_human_with_queryresult(cur, MOST_VERSATILE_ROLES_QUERY, MOST_VERSATILE_HUMAN_ROLES, "versatile in roles")
 
 
 def execute_counts(cur):
@@ -104,27 +113,32 @@ def execute_counts(cur):
 
 
 def execute_verify_creative_work_type(cur):
-    for human, count, work_types in all_verify_works_from_someone:
-        query = all_works_from_someone.format(human)
+    for human, count, work_types, roles in all_verify_works_from_someone:
+        query = ALL_WORKS_FROM_SOMEONE_QUERY.format(human)
         cur.execute(query)
         result = cur.fetchall()
-        work_found, rows_found = set(), 0
+        rows_found, works_found, roles_found = 0, set(), set()
         for row in result:
             rows_found += 1
-            work_type = row[2]
-            work_found.add(work_type)
+            work_type, role = row[2], row[3]
+            works_found.add(work_type)
+            roles_found.add(role)
         print("Asserting {} rows for {}  >= {}".format(rows_found, human, count))
         assert rows_found >= count
-        print("Asserting {} rows for {} contains {}".format(work_found, human, work_types))
-        assert all([work in work_found for work in work_types])
+        print("Asserting {} rows for {} contains {}".format(works_found, human, work_types))
+        assert all([work in works_found for work in work_types])
+        print("Asserting {} rows for {} contains {}".format(roles_found, human, roles))
+        assert all([role in roles_found for role in roles])
 
 
 def execute_tests(cur):
     print("Executing tests on data...")
     print("Executing test on tables size...")
-    execute_counts(cur)
+    #execute_counts(cur)
     print("Executing test on creative_work_type...")
-    execute_verify_creative_work_type(cur)
+    #execute_verify_creative_work_type(cur)
+    print("Executing aggregation tests")
+    execute_most_creative(cur)
 
 
 
